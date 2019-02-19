74f0a378d4a4f97ae9039a979f79a24f6e150a00
Samira Abnar <samiraabnar@gmail.com>
2019-02-19 18:40:46 +0100

    fix bug in plain trainer

--- distill/plain_sst_trainer.py
+++ distill/plain_sst_trainer.py
@@ -48,7 +48,7 @@ hparams = tf.app.flags.FLAGS
 class PlainSSTTrainer(object):
   def __init__(self, hparams, model_class):
     self.config = hparams
-    self.sst = SST("data/sst")
+    self.sst = SST("data/sst", pretrained_path=self.config.pretrained_embedding_path, embedding_size=self.config.embedding_dim)
     self.config.input_dim = len(self.sst.vocab)
 
     self.vocab = PretrainedVocab(self.config.data_path, self.config.pretrained_embedding_path,

e9ea2aaeaa1c392d851e13fd20161aa3a13cf47b
Samira Abnar <samiraabnar@gmail.com>
2019-03-27 10:41:45 +0100

    tuning for algorithmic

--- distill/algorithmic_trainer.py
+++ distill/algorithmic_trainer.py
@@ -135,10 +135,9 @@ class LSTMHparam(object):
     self.sent_rep_mode = sent_rep_mode
     self.clip_grad_norm = 0.  # i.e. no gradient clipping
     self.optimizer_adam_epsilon = 1e-9
-    self.learning_rate = 0.1
+    self.learning_rate = 0.02
     self.learning_rate_warmup_steps = 4000
     self.initializer_gain = 1.0
-    self.num_hidden_layers = 6
     self.initializer = "uniform_unit_scaling"
     self.weight_decay = 0.0
     self.optimizer_adam_beta1 = 0.9

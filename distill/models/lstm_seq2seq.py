869d395f089725608a927de8a0ad03370c2d7328
Samira Abnar <samiraabnar@gmail.com>
2019-05-01 15:20:55 +0200

    add non padded accuracy

--- distill/models/lstm_seq2seq.py
+++ distill/models/lstm_seq2seq.py
@@ -129,7 +129,7 @@ class LSTMSeq2Seq(object):
       output_mask = tf.cast(tf.sequence_mask(lstm_decoder_output_dic['outputs_lengths'], tf.shape(outputs)[1]), dtype=tf.int64)
 
       logits = self.output_embedding_layer.linear(outputs)
-      predictions = tf.cast(tf.argmax(logits, axis=-1) * output_mask, dtype=tf.int32)
+      predictions = tf.cast(tf.argmax(logits, axis=-1) * output_mask, dtype=tf.int64)
 
     return {'logits': logits,
             'output_mask': output_mask,

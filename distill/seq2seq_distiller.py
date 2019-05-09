f92d7d7982f03b71e68a77b121e348534a5b97a0
Samira <samira.abnar@gmail.com>
2019-05-09 15:43:47 +0200

    update scripts

--- distill/seq2seq_distiller.py
+++ distill/seq2seq_distiller.py
@@ -10,7 +10,8 @@ from distill.data_util.prep_ptb import PTB
 from distill.data_util.prep_sst import SST
 from distill.data_util.prep_wsj_parsing import ParseWSJ
 from distill.models.lstm_seq2seq import LSTMSeq2Seq, BidiLSTMSeq2Seq
-from distill.models.transformer import Transformer, UniversalTransformer
+from distill.models.transformer import Transformer, UniversalTransformer, EncodingTransformer, \
+  EncodingUniversalTransformer
 from distill.pipelines.distill_pipelines import Seq2SeqDistiller
 from distill.pipelines.seq2seq import Seq2SeqTrainer
 
@@ -22,63 +23,76 @@ tf.app.flags.DEFINE_string("log_dir", "logs", "")
 tf.app.flags.DEFINE_string("save_dir", None, "")
 
 tf.app.flags.DEFINE_string("teacher_model", "transformer", "")
+tf.app.flags.DEFINE_string("student_model", "transformer", "")
+
 tf.app.flags.DEFINE_boolean("train_teacher", True, "")
 tf.app.flags.DEFINE_boolean("train_student", False, "")
 tf.app.flags.DEFINE_boolean("distill_rep", False, "")
 tf.app.flags.DEFINE_boolean("distill_logit", True, "")
 
-tf.app.flags.DEFINE_string("student_model", "lstm", "")
 tf.app.flags.DEFINE_boolean("pretrain_teacher", True, "")
 tf.app.flags.DEFINE_integer("teacher_pretraining_iters", 100, "")
 tf.app.flags.DEFINE_string("rep_loss_mode", 'dot_product', "representation loss type (squared,softmax_cross_ent,sigmoid_cross_ent")
 
-tf.app.flags.DEFINE_boolean('train_embeddings', True, " False | True")
-tf.app.flags.DEFINE_string('attention_mechanism',None, 'attention_mechanism')
+tf.app.flags.DEFINE_string("exp_name", "trial", "")
+tf.app.flags.DEFINE_string("task_name", "sst", "sst | arithmatic | identity_binary| identity | addition| multiplication | sort | reverse")
+tf.app.flags.DEFINE_string("log_dir", "logs", "")
+tf.app.flags.DEFINE_string("save_dir", None, "")
+tf.app.flags.DEFINE_string("model", "transformer", "transformer | utransformer | lstm | bilstm")
+tf.app.flags.DEFINE_string("encoder_attention_dir", "top_down", "top_down | bottom_up")
 
+tf.app.flags.DEFINE_string("hidden_dims", 300, "")
+tf.app.flags.DEFINE_integer("encoder_depths", 2, "")
+tf.app.flags.DEFINE_integer("decoder_depths", 1, "")
 
-tf.app.flags.DEFINE_string("model_type", "", "")
-tf.app.flags.DEFINE_integer("student_hidden_dim", 128, "")
-tf.app.flags.DEFINE_integer("student_depth", 2, "")
-tf.app.flags.DEFINE_integer("teacher_hidden_dim", 128, "")
-tf.app.flags.DEFINE_integer("teacher_depth", 2, "")
 tf.app.flags.DEFINE_integer("input_dim", None, "")
 tf.app.flags.DEFINE_integer("output_dim", 1, "")
-tf.app.flags.DEFINE_string("student_attention_mechanism", None, "")
-tf.app.flags.DEFINE_string("teacher_attention_mechanism", None, "")
-tf.app.flags.DEFINE_string("sent_rep_mode", 'final', "")
-tf.app.flags.DEFINE_integer("number_of_heads", 2, "")
+tf.app.flags.DEFINE_integer("number_of_heads", 4, "")
 tf.app.flags.DEFINE_integer("ff_filter_size", 512, "")
 tf.app.flags.DEFINE_float("initializer_gain", 1.0, "")
 tf.app.flags.DEFINE_float("label_smoothing", 0.1, "")
+tf.app.flags.DEFINE_boolean('teacher_train_embeddings', True, " False | True")
+tf.app.flags.DEFINE_boolean('student_train_embeddings', True, " False | True")
 
-tf.app.flags.DEFINE_string("loss_type", "root_loss", "")
-tf.app.flags.DEFINE_float("input_dropout_keep_prob", 0.75, "")
-tf.app.flags.DEFINE_float("hidden_dropout_keep_prob", 0.25, "")
+tf.app.flags.DEFINE_string('teacher_sent_rep_mode', "final", "none | final | all")
+tf.app.flags.DEFINE_string('student_sent_rep_mode', "final", "none | final | all")
 
-tf.app.flags.DEFINE_float("learning_rate", 0.00001, "")
+tf.app.flags.DEFINE_string('teacher_attention_mechanism', None, 'attention_mechanism')
+tf.app.flags.DEFINE_string('student_attention_mechanism', None, 'attention_mechanism')
+
+
+tf.app.flags.DEFINE_float("teacher_input_dropout_keep_probs", 0.75, "")
+tf.app.flags.DEFINE_float("teacher_hidden_dropout_keep_prob", 0.5, "")
+tf.app.flags.DEFINE_float("student_input_dropout_keep_probs", 0.75, "")
+tf.app.flags.DEFINE_float("student_hidden_dropout_keep_prob", 0.5, "")
+
+
+tf.app.flags.DEFINE_float("teacher_learning_rate", 0.001, "")
+tf.app.flags.DEFINE_float("student_learning_rate", 0.001, "")
+
+tf.app.flags.DEFINE_boolean("decay_learning_rate", True, "True | False")
 tf.app.flags.DEFINE_float("l2_rate", 0.001, "")
 
-tf.app.flags.DEFINE_integer("batch_size", 32, "")
-tf.app.flags.DEFINE_integer("training_iterations", 30000, "")
 
-tf.app.flags.DEFINE_integer("vocab_size", 8000, "")
+tf.app.flags.DEFINE_integer("batch_size", 32, "")
+tf.app.flags.DEFINE_integer("training_iterations", 60000, "")
+
+tf.app.flags.DEFINE_integer("vocab_size", 3, "")
 tf.app.flags.DEFINE_integer("embedding_dim", 300, "embeddings dim")
 
-
-tf.app.flags.DEFINE_string("pretrained_embedding_path", "data/sst/filtered_glove.txt", "pretrained embedding path")
 tf.app.flags.DEFINE_string("data_path", "./data", "data path")
-
 
 hparams = tf.app.flags.FLAGS
 
 
 if __name__ == '__main__':
 
-
   Models = {"lstm": LSTMSeq2Seq,
             "bilstm": BidiLSTMSeq2Seq,
             "transformer": Transformer,
-            "utransformer": UniversalTransformer}
+            "utransformer": UniversalTransformer,
+            "enc_transformer": EncodingTransformer,
+            "enc_utransformer": EncodingUniversalTransformer}
 
 
   tasks = {'identity': AlgorithmicIdentityDecimal40('data/alg'),
@@ -97,53 +111,69 @@ if __name__ == '__main__':
   hparams.vocab_size = tasks[hparams.task_name].vocab_length
   hparams.output_dim = len(tasks[hparams.task_name].target_vocab)
 
-  transformer_params = TransformerHparam(input_dim=hparams.input_dim,
-                                         hidden_dim=hparams.teacher_hidden_dim,
-                                         output_dim=hparams.output_dim,
-                                         depth=hparams.teacher_depth,
-                                         number_of_heads=2,
-                                         ff_filter_size=512,
-                                         initializer_gain=hparams.initializer_gain,
-                                         batch_size=hparams.batch_size,
-                                         pretrained_embedding_path=hparams.pretrained_embedding_path,
-                                         input_dropout_keep_prob=hparams.input_dropout_keep_prob,
-                                         hidden_dropout_keep_prob=hparams.hidden_dropout_keep_prob,
-                                         vocab_size=hparams.vocab_size,
-                                         label_smoothing=hparams.label_smoothing,
-                                         encoder_self_attention_dir="top_down",
-                                         decoder_self_attention_dir="top_down",
-                                         decoder_cross_attention_dir="top_down",
-                                         train_embeddings=hparams.train_embeddings
-                                         )
+  PARAM_TYPES = {"lstm": LSTMHparam,
+            "bilstm": LSTMHparam,
+            "transformer": TransformerHparam,
+            "utransformer": TransformerHparam,
+            "enc_transformer": TransformerHparam,
+            "enc_utransformer": TransformerHparam}
 
-  lstm_params = LSTMHparam(input_dim=hparams.input_dim,
-                           hidden_dim=hparams.student_hidden_dim,
-                           output_dim=hparams.output_dim,
-                           depth=hparams.student_depth,
-                           number_of_heads=hparams.number_of_heads,
-                           ff_filter_size=hparams.ff_filter_size,
-                           initializer_gain=hparams.initializer_gain,
-                           batch_size=hparams.batch_size,
-                           pretrained_embedding_path=hparams.pretrained_embedding_path,
-                           input_dropout_keep_prob=hparams.input_dropout_keep_prob,
-                           hidden_dropout_keep_prob=hparams.hidden_dropout_keep_prob,
-                           vocab_size=hparams.vocab_size,
-                           label_smoothing=hparams.label_smoothing,
-                           attention_mechanism=None,
-                           sent_rep_mode=hparams.sent_rep_mode,
-                           embedding_dim=300,
-                           train_embeddings = hparams.train_embeddings,
-                           learning_rate=hparams.learning_rate)
+  teacher_params = PARAM_TYPES[hparams.teacher_model](input_dim=hparams.input_dim,
+                                                      output_dim=hparams.output_dim,
+                                                      hidden_dim=hparams.teacher_hidden_dim,
+                                                      encoder_depth=hparams.teacher_encoder_depth,
+                                                      decoder_depth=hparams.teacher_decoder_depth,
+                                                      number_of_heads=2,
+                                                      ff_filter_size=512,
+                                                      initializer_gain=hparams.initializer_gain,
+                                                      batch_size=hparams.batch_size,
+                                                      input_dropout_keep_prob=hparams.teacher_input_dropout_keep_prob,
+                                                      hidden_dropout_keep_prob=hparams.teacher_hidden_dropout_keep_prob,
+                                                      vocab_size=hparams.vocab_size,
+                                                      label_smoothing=hparams.teacher_label_smoothing,
+                                                      encoder_self_attention_dir=hparams.teacher_encoder_attention_dir,
+                                                      decoder_self_attention_dir="top_down",
+                                                      decoder_cross_attention_dir="top_down",
+                                                      train_embeddings=hparams.teacher_train_embeddings,
+                                                      attention_mechanism=None,
+                                                      sent_rep_mode=hparams.teacher_sent_rep_mode,
+                                                      embedding_dim=300,
+                                                      learning_rate=hparams.teacher_learning_rate
+                                                      )
 
+  student_params = PARAM_TYPES[hparams.teacher_model](input_dim=hparams.input_dim,
+                                                      output_dim=hparams.output_dim,
+                                                      hidden_dim=hparams.teacher_hidden_dim,
+                                                      encoder_depth=hparams.teacher_encoder_depth,
+                                                      decoder_depth=hparams.teacher_decoder_depth,
+                                                      number_of_heads=2,
+                                                      ff_filter_size=512,
+                                                      initializer_gain=hparams.initializer_gain,
+                                                      batch_size=hparams.batch_size,
+                                                      input_dropout_keep_prob=hparams.teacher_input_dropout_keep_prob,
+                                                      hidden_dropout_keep_prob=hparams.teacher_hidden_dropout_keep_prob,
+                                                      vocab_size=hparams.vocab_size,
+                                                      label_smoothing=hparams.teacher_label_smoothing,
+                                                      encoder_self_attention_dir=hparams.teacher_encoder_attention_dir,
+                                                      decoder_self_attention_dir="top_down",
+                                                      decoder_cross_attention_dir="top_down",
+                                                      train_embeddings=hparams.teacher_train_embeddings,
+                                                      attention_mechanism=None,
+                                                      sent_rep_mode=hparams.teacher_sent_rep_mode,
+                                                      embedding_dim=300,
+                                                      learning_rate=hparams.teacher_learning_rate
+                                                      )
 
 
   model_params = {"transformer": transformer_params,
                   "utransformer": transformer_params,
                   "lstm": lstm_params,
-                  "bilstm": lstm_params}
-
+                  "bilstm": lstm_params,
+                  "enc_transformer":transformer_params,
+                  "enc_utransformer": transformer_params,}
 
   hparams.model_type ='_'.join([hparams.teacher_model,'to',hparams.student_model])
+
   if hparams.save_dir is None:
     hparams.save_dir = os.path.join(hparams.log_dir, hparams.task_name, '_'.join(
       [hparams.rep_loss_mode, hparams.model_type, hparams.loss_type, 'std_depth' + str(hparams.student_depth),
@@ -151,7 +181,7 @@ if __name__ == '__main__':
        'teacher_hidden_dim' + str(hparams.teacher_hidden_dim), hparams.exp_name]))
 
   student_model = Models[hparams.student_model](model_params[hparams.student_model],
-                                task= tasks[hparams.task_name],
+                                task=tasks[hparams.task_name],
                                 scope=hparams.student_model+"_student")
   teacher_model = Models[hparams.teacher_model](model_params[hparams.teacher_model],
                                         task=tasks[hparams.task_name],

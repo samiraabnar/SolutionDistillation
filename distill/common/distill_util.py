758637930d66a632e73a92e104b094bbf445adc3
Samira Abnar <samiraabnar@gmail.com>
2019-05-14 22:11:19 +0200

    avoiding Nan in computing rep sim

--- distill/common/distill_util.py
+++ distill/common/distill_util.py
@@ -68,10 +68,10 @@ def dot_product_sim(a, b):
   b = tf.reshape(b, [-1, tf.shape(b)[-1]])
 
   a_norm = tf.expand_dims(tf.norm(a, axis=-1), -1)
-  a = a / a_norm
+  a = a / (a_norm+0.000000001)
 
   b_norm = tf.expand_dims(tf.norm(b, axis=-1), -1)
-  b = b / b_norm
+  b = b / (b_norm+0.000000001)
 
   sim_mat = tf.matmul(a, b,
                      transpose_b=True  # transpose second matrix

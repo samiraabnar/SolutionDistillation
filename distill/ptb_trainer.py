ad6647b54208ad4256ad6a8a2ad57218f008f4f0
Samira Abnar <samiraabnar@gmail.com>
2019-08-15 14:51:56 +0200

    update old codes

--- distill/ptb_trainer.py
+++ distill/ptb_trainer.py
@@ -19,6 +19,8 @@ tf.app.flags.DEFINE_string("save_dir", None, "")
 tf.app.flags.DEFINE_string("model_type", "plain_lstm", "")
 tf.app.flags.DEFINE_integer("hidden_dim", 128, "")
 tf.app.flags.DEFINE_integer("output_dim", 8000, "")
+tf.app.flags.DEFINE_integer("input_dim", 8000, "")
+
 tf.app.flags.DEFINE_integer("depth", 1, "")
 tf.app.flags.DEFINE_string("attention_mechanism", None, "")
 tf.app.flags.DEFINE_string("sent_rep_mode", 'all', "all| final| ")

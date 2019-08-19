437b5208ed9869837be1bb85ec9e3c7d454f8347
Samira Abnar <samiraabnar@gmail.com>
2019-08-19 18:52:16 +0200

    update script

--- lisa_scripts/lisa_run_lm.bat
+++ lisa_scripts/lisa_run_lm.bat
@@ -43,11 +43,11 @@ export PYTHONPATH=$PYTHONPATH:$HEAD_DIR
 
 CUDA_VISIBLE_DEVICES=0 python $CODE_DIR/lm_trainer.py \
 --log_dir="$TMP_LOGS_DIR" --data_dir="$TMP_DATA_DIR" \
---batch_size=64 --hidden_dim=728 --embedding_dim=728 \
---learning_rate=0.0002 \
+--batch_size=32 --hidden_dim=728 --embedding_dim=728 \
+--learning_rate=0.001 \
 --hidden_dropout_keep_prob=0.8 --input_dropout_keep_prob=0.6 \
 --task_name=ptb_lm \
---exp_name b64_drop8-6
+--exp_name b32_drop8-6
 
 #Copy input file to scratch
 #cp -r  "$TMP_DATA_DIR"/* "$DATA_DIR"/

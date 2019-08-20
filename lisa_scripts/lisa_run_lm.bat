a51cd07571940f3196cf3bc4e484d0e314fa6115
Samira Abnar <samiraabnar@gmail.com>
2019-08-20 12:04:29 +0200

    update lisa script

--- lisa_scripts/lisa_run_lm.bat
+++ lisa_scripts/lisa_run_lm.bat
@@ -43,11 +43,11 @@ export PYTHONPATH=$PYTHONPATH:$HEAD_DIR
 
 CUDA_VISIBLE_DEVICES=0 python $CODE_DIR/lm_trainer.py \
 --log_dir="$TMP_LOGS_DIR" --data_dir="$TMP_DATA_DIR" \
---batch_size=16 --hidden_dim=728 --embedding_dim=728 \
+--batch_size=64 --hidden_dim=1024 --embedding_dim=1024 \
 --learning_rate=0.001 \
 --hidden_dropout_keep_prob=0.8 --input_dropout_keep_prob=0.8 \
 --task_name=sent_wiki \
---exp_name b16_drop8-8
+--exp_name b64_drop8-8
 
 #Copy input file to scratch
 #cp -r  "$TMP_DATA_DIR"/* "$DATA_DIR"/

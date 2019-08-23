f1224c609550f4f0a76f730b82b8a6ec7b981110
Samira Abnar <samiraabnar@gmail.com>
2019-08-23 10:13:52 +0200

    lisa script

--- lisa_scripts/lisa_run_lm.bat
+++ lisa_scripts/lisa_run_lm.bat
@@ -44,8 +44,8 @@ export PYTHONPATH=$PYTHONPATH:$HEAD_DIR
 CUDA_VISIBLE_DEVICES=0 python $CODE_DIR/lm_trainer.py \
 --log_dir="$TMP_LOGS_DIR" --data_dir="$TMP_DATA_DIR" \
 --batch_size=64 --hidden_dim=1024 --embedding_dim=1024 \
---learning_rate=0.001 \
---hidden_dropout_keep_prob=0.8 --input_dropout_keep_prob=0.8 \
+--learning_rate=0.005 \
+--hidden_dropout_keep_prob=0.7 --input_dropout_keep_prob=0.7 \
 --task_name=sent_wiki \
 --exp_name drop8-8_run2
 

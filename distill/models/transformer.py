3d208285e21f11de1aec12278e831510569af33f
Samira Abnar <samiraabnar@gmail.com>
2019-08-28 12:44:32 +0200

    add decoding transformer

--- distill/models/transformer.py
+++ distill/models/transformer.py
@@ -88,7 +88,6 @@ class TransformerEncoder(object):
 
     return self.output_normalization.apply(encoder_inputs, is_train), encoder_inputs_presence
 
-
 class TransformerDecoder(object):
   def __init__(self, hidden_dim, number_of_heads, depth, ff_filter_size,
                attention_dropout_keepprob,
@@ -159,6 +158,7 @@ class TransformerDecoder(object):
   def apply(self, inputs, encoder_outputs, decoder_self_attention_bias, attention_bias, encoder_outputs_presence=None,
             cache=None, is_train=True,  reuse=tf.AUTO_REUSE,
             target_length=None, dic_to_save_weights=None):
+
     decoder_inputs = inputs
     with tf.variable_scope(self.scope, reuse=reuse):
       for n, layer in enumerate(self.layers):
@@ -170,17 +170,19 @@ class TransformerDecoder(object):
         enc_dec_attention = layer[1]
         feed_forward_network = layer[2]
 
+
         decoder_inputs, _ = self_attention_layer.apply(x=decoder_inputs, y=decoder_inputs, layer_id=n, is_train=is_train,
                                                       bias=decoder_self_attention_bias, cache=layer_cache,
                                                       dic_to_save_weights=dic_to_save_weights)
-        decoder_inputs, _ = enc_dec_attention.apply(x=decoder_inputs, y=encoder_outputs, layer_id=n, is_train=is_train,
-                                                       y_presence=encoder_outputs_presence,
-                                                       bias=attention_bias,
-                                                       dic_to_save_weights=dic_to_save_weights)
+        if encoder_outputs is not None:
+          decoder_inputs, _ = enc_dec_attention.apply(x=decoder_inputs, y=encoder_outputs, layer_id=n, is_train=is_train,
+                                                         y_presence=encoder_outputs_presence,
+                                                         bias=attention_bias,
+                                                         dic_to_save_weights=dic_to_save_weights)
+
         decoder_inputs,_ = feed_forward_network.apply(x=decoder_inputs, is_train=is_train)
 
     return self.output_normalization.apply(decoder_inputs, is_train)
-
 
 class UniversalTransformerEncoder(TransformerEncoder):
   def __init__(self, hidden_dim, number_of_heads, depth, ff_filter_size,
@@ -228,7 +230,6 @@ class UniversalTransformerEncoder(TransformerEncoder):
       # Create final layer normalization layer.
       self.output_normalization = LayerNormalization(self.hidden_dim)
       self.output_normalization.create_vars()
-
 
 class UniversalTransformerDecoder(TransformerDecoder):
   def __init__(self, hidden_dim, number_of_heads, depth, ff_filter_size, attention_dropout_keepprob,
@@ -293,7 +294,6 @@ class UniversalTransformerDecoder(TransformerDecoder):
       self.output_normalization = LayerNormalization(self.hidden_dim)
       self.output_normalization.create_vars()
 
-
 class Transformer(object):
   """Transformer model for sequence to sequence data.
   Implemented as described in: https://arxiv.org/pdf/1706.03762.pdf
@@ -323,7 +323,7 @@ class Transformer(object):
 
   def create_vars(self, reuse=False,pretrained_embeddings=None):
     self.initializer = tf.variance_scaling_initializer(
-      self.initializer_gain, mode="fan_avg", distribution="uniform")
+      self.initializer_gain, mode="fan_avg", distribution="normal")
 
     with tf.variable_scope(self.scope, initializer=self.initializer, reuse=tf.AUTO_REUSE):
 
@@ -562,7 +562,6 @@ class Transformer(object):
 
     return {"outputs": top_decoded_ids, "scores": top_scores}
 
-
 class UniversalTransformer(Transformer):
 
   def __init__(self, hparams, task, scope="Transformer"):
@@ -740,7 +739,6 @@ class EncodingTransformer(object):
 
       return tf.expand_dims(outputs, axis=1)
 
-
 class DecodingTransformer(object):
   """Transformer model for sequence classification.
   Implemented as described in: https://arxiv.org/pdf/1706.03762.pdf
@@ -764,10 +762,11 @@ class DecodingTransformer(object):
     self.scope = scope
     self.task = task
     self.eos_id = self.task.eos_id
-
-  def create_vars(self, reuse=False, pretrained_embeddings=None):
     self.initializer = tf.variance_scaling_initializer(
       self.initializer_gain, mode="fan_avg", distribution="normal")
+
+  def create_vars(self, reuse=False, pretrained_embeddings=None):
+
 
     with tf.variable_scope(self.scope, initializer=self.initializer, reuse=tf.AUTO_REUSE):
 
@@ -808,22 +807,12 @@ class DecodingTransformer(object):
     inputs, targets, inputs_lengths, targets_lengths = examples
 
     with tf.variable_scope(self.scope, initializer=self.initializer, reuse=reuse):
-      if self.hparams.cls_token:
-        inputs = tf.concat([tf.expand_dims(tf.ones(tf.shape(inputs)[:-1], dtype=tf.int64), axis=-1) * \
-                            self.task.word2id[self.task.cls_token],
-                            inputs], axis=1)
-        tf.logging.info("inputs shape")
-        tf.logging.info(inputs)
-
       attention_bias = get_padding_bias(inputs)
-
-      encoder_outputs, encoder_outputs_presence = self.encode(inputs, attention_bias, is_train,
-                                                              dic_to_save_weights=dic_to_save_weights)
-      outputs = self.decode(encoder_outputs=encoder_outputs,
-                            encoder_outputs_presence=encoder_outputs_presence,
+      outputs = self.decode(targets=inputs,
+                            encoder_outputs=None,
+                            attention_bias=attention_bias,
                             is_train=is_train, dic_to_save_weights=dic_to_save_weights)
 
-      # outputs  = self.output_projections_layer.apply(outputs)
       logits = self.output_embedding_layer.linear(outputs)
       predictions = tf.argmax(logits, axis=-1)
 
@@ -834,33 +823,10 @@ class DecodingTransformer(object):
               'trainable_vars': tf.trainable_variables(scope=self.scope),
               }
 
-  def encode(self, inputs, attention_bias, is_train=True, dic_to_save_weights=None):
-    """Generate continuous representation for inputs.
-    Args:
-      inputs: int tensor with shape [batch_size, input_length].
-      attention_bias: float tensor with shape [batch_size, 1, 1, input_length]
-    Returns:
-      float tensor with shape [batch_size, input_length, hidden_size]
-    """
-    with tf.variable_scope("encode", reuse=tf.AUTO_REUSE):
-      embedded_inputs = self.input_embedding_layer.apply(inputs)
-      inputs_padding = get_padding(inputs)
-
-      if is_train:
-        embedded_inputs = tf.nn.dropout(embedded_inputs, keep_prob=self.hparams.input_dropout_keep_prob)
-
-      with tf.name_scope("add_pos_encoding"):
-        length = tf.shape(embedded_inputs)[1]
-        pos_encoding = get_position_encoding(
-          length, self.hidden_dim)
-        encoder_inputs = embedded_inputs + pos_encoding
-
-
-
-      return self.encoder_stack.apply(encoder_inputs, attention_bias, inputs_padding, is_train,
-                                      dic_to_save_weights=dic_to_save_weights)
-
-  def decode(self, encoder_outputs, encoder_outputs_presence=None, is_train=True, dic_to_save_weights=None):
+  def decode(self, targets, encoder_outputs, attention_bias, encoder_outputs_presence=None,
+             is_train=True,
+             target_length=None,
+             dic_to_save_weights=None):
     """Generate logits for each value in the target sequence.
     Args:
       targets: target values for the output sequence.
@@ -872,16 +838,30 @@ class DecodingTransformer(object):
       float32 tensor with shape [batch_size, target_length, vocab_size]
     """
     with tf.name_scope("decode"):
-      if self.hparams.cls_token:
-        outputs = encoder_outputs[:, 0]
-      else:
-        if encoder_outputs_presence is not None:
-          outputs = tf.reduce_sum(encoder_outputs * encoder_outputs_presence, axis=1)
-        else:
-          outputs = tf.reduce_mean(encoder_outputs, axis=1)
+      # Prepare inputs to decoder layers by shifting targets, adding positional
+      # encoding and applying dropout.
+      decoder_inputs = self.input_embedding_layer.apply(targets)
+      with tf.name_scope("shift_targets"):
+        # Shift targets to the right, and remove the last element
+        decoder_inputs = tf.pad(
+          decoder_inputs, [[0, 0], [1, 0], [0, 0]])[:, :-1, :]
+      with tf.name_scope("add_pos_encoding"):
+        length = tf.shape(decoder_inputs)[1]
+        decoder_inputs += get_position_encoding(
+          length, self.hidden_dim)
 
-      return tf.expand_dims(outputs, axis=1)
+      decoder_self_attention_bias = get_decoder_self_attention_bias(
+        length)
 
+      outputs = self.decoder_stack.apply(
+        inputs=decoder_inputs,
+        encoder_outputs=encoder_outputs,
+        decoder_self_attention_bias=decoder_self_attention_bias,
+        attention_bias=attention_bias,
+        encoder_outputs_presence=encoder_outputs_presence,
+        target_length=None)
+
+      return outputs
 
 class EncodingUniversalTransformer(EncodingTransformer):
   """Transformer model for sequence to sequence data.
@@ -936,8 +916,7 @@ class EncodingUniversalTransformer(EncodingTransformer):
 
       self.encoder_stack.create_vars(reuse=tf.AUTO_REUSE)
 
-
-class DecodingUniversalTransformer(EncodingTransformer):
+class DecodingUniversalTransformer(DecodingTransformer):
   """Transformer model for sequence to sequence data.
   Implemented as described in: https://arxiv.org/pdf/1706.03762.pdf
   The Transformer model consists of an encoder and decoder. The input is an int
@@ -978,16 +957,15 @@ class DecodingUniversalTransformer(EncodingTransformer):
       else:
         self.output_embedding_layer = self.input_embedding_layer
 
-      self.encoder_stack = UniversalTransformerEncoder(self.hidden_dim, self.number_of_heads, self.encoder_depth,
-                                                       self.ff_filter_size,
-                                                       attention_dropout_keepprob=self.attention_dropout_keepprob,
-                                                       relu_dropout_keepprob=self.relu_dropout_keepprob,
-                                                       postprocess_dropout_keepprob=self.postprocess_dropout_keepprob,
-                                                       self_attention_dir=self.hparams.encoder_self_attention_dir,
-                                                       scope="UniversalTransformerEncoder")
+      self.decoder_stack = TransformerDecoder(self.hidden_dim, self.number_of_heads, self.encoder_depth,
+                                              self.ff_filter_size,
+                                              attention_dropout_keepprob=self.attention_dropout_keepprob,
+                                              relu_dropout_keepprob=self.relu_dropout_keepprob,
+                                              postprocess_dropout_keepprob=self.postprocess_dropout_keepprob,
+                                              self_attention_dir=self.hparams.encoder_self_attention_dir,
+                                              scope="UniversalTransformerDecoder")
 
-      self.encoder_stack.create_vars(reuse=tf.AUTO_REUSE)
-
+      self.decoder_stack.create_vars(reuse=tf.AUTO_REUSE)
 
 class EncodingUniversalTransformerWithLocalBias(EncodingUniversalTransformer):
   """Transformer model for sequence to sequence data.
@@ -1025,7 +1003,7 @@ if __name__ == '__main__':
 
   dataset = tf.data.TFRecordDataset(bin_iden.get_tfrecord_path(mode="train"))
   dataset = dataset.map(bin_iden.parse_examples)
-  dataset = dataset.padded_batch(32, padded_shapes=bin_iden.get_padded_shapes())
+  dataset = dataset.padded_batch(1, padded_shapes=bin_iden.get_padded_shapes())
   iterator = dataset.make_initializable_iterator()
 
   example = iterator.get_next()
@@ -1075,7 +1053,7 @@ if __name__ == '__main__':
       self.cls_token = True
 
 
-  transformer = EncodingTransformer(Config(),
+  transformer = DecodingUniversalTransformer(Config(),
                             task=bin_iden,
                             scope="Transformer")
   transformer.create_vars(reuse=False)
@@ -1090,7 +1068,7 @@ if __name__ == '__main__':
   scaffold = tf.train.Scaffold(local_init_op=tf.group(tf.local_variables_initializer(),
                                                       iterator.initializer))
   with tf.train.MonitoredTrainingSession(checkpoint_dir='logs/test_transformer', scaffold=scaffold) as sess:
-    for _ in np.arange(10):
+    for _ in np.arange(1):
       inp, targ, pred = sess.run([inputs, targets, predictions])
 
       print(inp)

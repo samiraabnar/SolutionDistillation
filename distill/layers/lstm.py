c0b36d3efa6aa7054b75f5f1e75807502fdfbc0f
Samira Abnar <samiraabnar@gmail.com>
2019-09-02 10:29:21 +0200

    refactor prediction

--- distill/layers/lstm.py
+++ distill/layers/lstm.py
@@ -117,105 +117,107 @@ class LSTM(object):
     return the_cell
 
   def predict(self, compute_decoding_step_input_fn, inputs_length, input_embedding_layer, output_embedding_layer, eos_id,
-              target_length=None, init_state=None, is_train=True, initial_inputs=None):
+              target_length=None, init_state=None, is_train=False, initial_inputs=None):
     batch_size = tf.shape(inputs_length)[0]
     with tf.variable_scope(self.scope, reuse=tf.AUTO_REUSE):
-      # Run the data through the RNN layers
-      with tf.variable_scope("LSTM_Cells", reuse=tf.AUTO_REUSE):
-        the_cell = self.multi_lstm_cell
-        if is_train:
-          the_cell = self.multi_dropout_lstm_cell
 
-        if init_state is None:
-          init_state = the_cell.zero_state(batch_size, tf.float32)
+      # Choose the cell with or without dropout
+      the_cell = self.multi_lstm_cell
+      if is_train:
+        the_cell = self.multi_dropout_lstm_cell
 
-        all_outputs_tensor_array = tf.TensorArray(dtype=tf.float32, size=0,
-                                     dynamic_size=True,
-                                     clear_after_read=False,
-                                     infer_shape=True)
-        sampled_predictions_tensor_array = tf.TensorArray(dtype=tf.int64, size=0,
-                                                  dynamic_size=True,
-                                                  clear_after_read=False,
-                                                  infer_shape=True)
-
-        # This loop gets called once for every "timestep" and obtains one column of the input data
-        def lstm_loop(sampled_predictions, output_lengths, all_outputs, last_lstm_prediction,last_state, finish_flags, step):
-
-
-          last_lstm_prediction = tf.expand_dims(last_lstm_prediction, 1)
-          last_lstm_prediction_logits = output_embedding_layer.linear(last_lstm_prediction)
-
-          prediction = tf.random.multinomial(logits=tf.squeeze(last_lstm_prediction_logits)/10,
-                                             num_samples=1)
-          sampled_predictions.write(step, prediction)
-
-          embedded_prediction = input_embedding_layer.apply(prediction)
-          embedded_prediction = embedded_prediction[:,-1,:]
-
-          current_step_input = compute_decoding_step_input_fn(embedded_prediction)
-          if current_step_input is not None:
-            cell_input = tf.concat([embedded_prediction, current_step_input], axis=-1)
-          else:
-            cell_input = embedded_prediction
-
-          lstm_prediction, state = the_cell(cell_input, last_state)
-
-          all_outputs = all_outputs.write(step, lstm_prediction)
-          finish_flags = tf.logical_or(finish_flags,tf.equal(prediction[:,-1],eos_id))
-          output_lengths = output_lengths + tf.cast( tf.logical_not(finish_flags), dtype=tf.int32)*1
-
-          return sampled_predictions, output_lengths, all_outputs, lstm_prediction, state, finish_flags, tf.add(step, 1)
-
-        if target_length is None:
-          timesteps = tf.reduce_max(inputs_length)
-        else:
-          timesteps = target_length
-
-        for_each_time_step = lambda s, l, c, a, b, f, step: tf.logical_and(
-          tf.less(tf.cast(step, dtype=tf.int32), tf.cast(timesteps, dtype=tf.int32)),
-          tf.logical_not(tf.reduce_all(f)))
-
-        initial_outputs = tf.zeros([batch_size, self.hidden_dim])
-        if initial_inputs is not None:
-          initial_outputs = output_embedding_layer.apply(initial_inputs)
-
-        init_finish = tf.cast(tf.zeros(batch_size, dtype=tf.int64), dtype=tf.bool)
-        init_output_lengths = tf.zeros(batch_size, dtype=tf.int32)
-        samples, output_lengths, all_outputs, final_prediction, lstm_state, _, _ = tf.while_loop(for_each_time_step, lstm_loop,
-                                                       (sampled_predictions_tensor_array, init_output_lengths, all_outputs_tensor_array,
-                                                        initial_outputs, init_state,init_finish, 0),
-                                                       parallel_iterations=32)
-
-
-        lstm_outputs = tf.transpose(all_outputs.stack(),[1,0,2])
-        samples = tf.transpose(samples.stack(), [1, 0, 2])
-
-
-      if self.attention_mechanism is not None:
-        with tf.variable_scope("Attention", reuse=tf.AUTO_REUSE):
-          lstm_outputs = self.attention.apply(lstm_outputs, inputs_length, is_train)
-
-
-      bach_indices = tf.expand_dims(tf.range(batch_size), 1)
-      root_indices = tf.concat([bach_indices, tf.expand_dims(tf.cast(inputs_length - 1, dtype=tf.int32), 1)], axis=-1)
-
-      # Sum over all representations for each sentence!
-      inputs_mask = tf.expand_dims(tf.cast(tf.sequence_mask(inputs_length), tf.float32),-1)
-
-      if self.sent_rep_mode == "all":
-        sentence_reps = tf.reduce_sum(lstm_outputs * inputs_mask, axis=1) / tf.expand_dims(tf.cast(inputs_length, tf.float32), -1)
-      elif self.sent_rep_mode == "final":
-        sentence_reps = tf.gather_nd(lstm_outputs, root_indices)
+      # Set maximum sequence Length
+      if target_length is None:
+        timesteps = tf.reduce_max(inputs_length)
       else:
-        sentence_reps = tf.reduce_sum(lstm_outputs * inputs_mask, axis=1)
+        timesteps = target_length
+
+      # TensorArray to keep final LSTM outputs
+      all_outputs_tensor_array = tf.TensorArray(dtype=tf.float32, size=0,
+                                   dynamic_size=True,
+                                   clear_after_read=False,
+                                   infer_shape=True)
+
+      # TensorArray to keep predictions
+      sampled_predictions_tensor_array = tf.TensorArray(dtype=tf.int64, size=0,
+                                                dynamic_size=True,
+                                                clear_after_read=False,
+                                                infer_shape=True)
+
+      # Loop condition
+      for_each_time_step = lambda s, l, c, a, b, f, step: tf.logical_and(
+        tf.less(tf.cast(step, dtype=tf.int32), tf.cast(timesteps, dtype=tf.int32)),
+        tf.logical_not(tf.reduce_all(f)))
+
+      # This loop gets called once for every "timestep" and obtains one column of the input data
+      def lstm_loop(sampled_predictions,
+                    last_prediction, last_state,
+                    output_lengths, finish_flags, step):
+        """
+
+        :param sampled_predictions: tensor array to save predictions
+        :param last prediction: [batch_size,1,1]
+        :param last_state:  [batch_size, hidden_dim]
+        :param output_lengths: [batch size, 1]
+        :param finish_flags: [batch_size,1]
+        :param step: scalar integer
+        :return:
+        """
+
+        embedded_prediction = input_embedding_layer.apply(last_prediction)
+
+        current_step_input = compute_decoding_step_input_fn(embedded_prediction)
+        if current_step_input is not None:
+          cell_input = tf.concat([embedded_prediction, current_step_input], axis=-1)
+        else:
+          cell_input = embedded_prediction
+
+        _, state = the_cell(cell_input, last_state)
+
+        logits = output_embedding_layer.linear(state)
+        prediction = tf.random.multinomial(logits=tf.squeeze(logits),
+                                           num_samples=1)
+        tf.logging.info("prediction shape")
+        tf.logging.info(prediction)
+        sampled_predictions.write(step, prediction)
+
+        finish_flags = tf.logical_or(finish_flags,tf.equal(prediction[:,-1],eos_id))
+        output_lengths = output_lengths + tf.cast( tf.logical_not(finish_flags), dtype=tf.int32)*1
+
+        return sampled_predictions, prediction, state, output_lengths, finish_flags, tf.add(step, 1)
 
 
+
+      # Initial outpus are zero unless initial inputs are not None (e.g. we have a start of sentence token)
+      # initial_outputs = tf.zeros([batch_size, self.hidden_dim])
+      # if initial_inputs is not None:
+      #   initial_outputs = output_embedding_layer.apply(initial_inputs)
+
+      # Initially none of the sentences are finished
+      init_finish = tf.cast(tf.zeros(batch_size, dtype=tf.int64), dtype=tf.bool)
+
+      #Initially generated sentence lengths are all zero
+      init_output_lengths = tf.zeros(batch_size, dtype=tf.int32)
+
+      # If initial state is None set it to zero
+      if init_state is None:
+        init_state = the_cell.zero_state(batch_size, tf.float32)
+
+      samples, final_prediction, lstm_state, output_lengths, _, _ = tf.while_loop(for_each_time_step,
+                                                                                               lstm_loop,
+                                                                                               (sampled_predictions_tensor_array,
+                                                                                                initial_inputs,
+                                                                                                init_output_lengths,
+                                                                                                init_state,
+                                                                                                init_finish,
+                                                                                                0),
+                                                                                                parallel_iterations=32)
+
+
+      samples = tf.transpose(samples.stack(), [1, 0, 2])
 
 
     return {
-            'raw_outputs': lstm_outputs,
-            'sents_reps': sentence_reps,
-            'seq_outputs': lstm_outputs,
             'final_state': lstm_state,
             'outputs_lengths': output_lengths,
             'samples': samples

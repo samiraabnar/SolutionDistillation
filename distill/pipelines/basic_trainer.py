c1808592dddbb9ea78db16414d8c27de5bff7b45
Samira Abnar <samiraabnar@gmail.com>
2019-09-05 15:32:53 +0200

    change in training

--- distill/pipelines/basic_trainer.py
+++ distill/pipelines/basic_trainer.py
@@ -23,7 +23,7 @@ class Trainer(object):
 
       if self.config.decay_learning_rate:
         decay_learning_rate = tf.train.exponential_decay(base_learning_rate, self.global_step,
-                                                       10000, 0.98, staircase=False)
+                                                       1000, 0.98, staircase=False)
         # decay_learning_rate = tf.train.cosine_decay_restarts(base_learning_rate, self.global_step,
         #                       1000)
       else:

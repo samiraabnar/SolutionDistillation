6500dad7c2f4b5b0e5e8cc8b4117ef836dc5fa90
Samira Abnar <samiraabnar@gmail.com>
2019-09-14 11:52:05 +0200

    working on mnist

--- distill/models/lenet5.py
+++ distill/models/lenet5.py
@@ -89,66 +89,66 @@ class LeNet5(object):
 
 
 
-if __name__ == '__main__':
-
-  class Config(object):
-    def __init__(self):
-      self.output_dim = 10
-      self.input_dim = 728
-      self.encoder_depth = 1
-      self.decoder_depth = 1
-      self.sent_rep_mode = "all"
-      self.scope = "lenet5"
-      self.batch_size = 64
-      self.input_dropout_keep_prob = 1.0
-      self.hidden_dropout_keep_prob = 1.0
-      self.number_of_heads = 2
-      self.filter_size = 5
-      self.initializer_gain = 1.0
-      self.label_smoothing = 0.1
-      self.clip_grad_norm = 0.  # i.e. no gradient clipping
-      self.optimizer_adam_epsilon = 1e-9
-      self.learning_rate = 0.001
-      self.learning_rate_warmup_steps = 1000
-      self.initializer_gain = 1.0
-      self.initializer = "uniform_unit_scaling"
-      self.weight_decay = 0.0
-      self.optimizer_adam_beta1 = 0.9
-      self.optimizer_adam_beta2 = 0.98
-      self.num_sampled_classes = 0
-      self.label_smoothing = 0.1
-      self.clip_grad_norm = 0.  # i.e. no gradient clipping
-      self.optimizer_adam_epsilon = 1e-9
-      self.alpha = 1
-
-
-  tf.logging.set_verbosity(tf.logging.INFO)
-  bin_iden = Mnist1D('data/mnist1d')
-
-
-  dataset = tf.data.TFRecordDataset(bin_iden.get_tfrecord_path(mode="train"))
-  dataset = dataset.map(bin_iden.parse_examples)
-  dataset = dataset.padded_batch(5, padded_shapes=bin_iden.get_padded_shapes())
-  iterator = dataset.make_initializable_iterator()
-
-  example = iterator.get_next()
-  inputs, targets, inputs_lengths, targets_length = example
-
-  lenet = LeNet5(Config(),
-                       task=bin_iden,
-                       scope="lenet5")
-  lenet.create_vars(reuse=False)
-
-  outputs = lenet.apply(example, target_length=bin_iden.target_length, is_train=True)
-  outputs = outputs["logits"]
-
-  global_step = tf.train.get_or_create_global_step()
-  scaffold = tf.train.Scaffold(local_init_op=tf.group(tf.local_variables_initializer(),
-                                                      iterator.initializer))
-  with tf.train.MonitoredTrainingSession(checkpoint_dir='logs/test_lenet', scaffold=scaffold) as sess:
-    for _ in np.arange(1):
-      inp, targ, outp = sess.run([inputs, targets, outputs])
-
-      print(inp.shape)
-      print(targ.shape)
-      print(outp.shape)
+# if __name__ == '__main__':
+#
+#   class Config(object):
+#     def __init__(self):
+#       self.output_dim = 10
+#       self.input_dim = 728
+#       self.encoder_depth = 1
+#       self.decoder_depth = 1
+#       self.sent_rep_mode = "all"
+#       self.scope = "lenet5"
+#       self.batch_size = 64
+#       self.input_dropout_keep_prob = 1.0
+#       self.hidden_dropout_keep_prob = 1.0
+#       self.number_of_heads = 2
+#       self.filter_size = 5
+#       self.initializer_gain = 1.0
+#       self.label_smoothing = 0.1
+#       self.clip_grad_norm = 0.  # i.e. no gradient clipping
+#       self.optimizer_adam_epsilon = 1e-9
+#       self.learning_rate = 0.001
+#       self.learning_rate_warmup_steps = 1000
+#       self.initializer_gain = 1.0
+#       self.initializer = "uniform_unit_scaling"
+#       self.weight_decay = 0.0
+#       self.optimizer_adam_beta1 = 0.9
+#       self.optimizer_adam_beta2 = 0.98
+#       self.num_sampled_classes = 0
+#       self.label_smoothing = 0.1
+#       self.clip_grad_norm = 0.  # i.e. no gradient clipping
+#       self.optimizer_adam_epsilon = 1e-9
+#       self.alpha = 1
+#
+#
+#   tf.logging.set_verbosity(tf.logging.INFO)
+#   bin_iden = Mnist1D('data/mnist1d')
+#
+#
+#   dataset = tf.data.TFRecordDataset(bin_iden.get_tfrecord_path(mode="train"))
+#   dataset = dataset.map(bin_iden.parse_examples)
+#   dataset = dataset.padded_batch(5, padded_shapes=bin_iden.get_padded_shapes())
+#   iterator = dataset.make_initializable_iterator()
+#
+#   example = iterator.get_next()
+#   inputs, targets, inputs_lengths, targets_length = example
+#
+#   lenet = LeNet5(Config(),
+#                        task=bin_iden,
+#                        scope="lenet5")
+#   lenet.create_vars(reuse=False)
+#
+#   outputs = lenet.apply(example, target_length=bin_iden.target_length, is_train=True)
+#   outputs = outputs["logits"]
+#
+#   global_step = tf.train.get_or_create_global_step()
+#   scaffold = tf.train.Scaffold(local_init_op=tf.group(tf.local_variables_initializer(),
+#                                                       iterator.initializer))
+#   with tf.train.MonitoredTrainingSession(checkpoint_dir='logs/test_lenet', scaffold=scaffold) as sess:
+#     for _ in np.arange(1):
+#       inp, targ, outp = sess.run([inputs, targets, outputs])
+#
+#       print(inp.shape)
+#       print(targ.shape)
+#       print(outp.shape)
